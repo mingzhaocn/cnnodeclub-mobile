6f5c317012e3d8cd43a8f4325d143cfd1481d1f4
songmingzhao <songmz@songmingzhaodeMacBook-Pro.local>
2017-08-14 19:09:00 +0800

    add fastclick

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import 'normalize.css';
+const attachFastClick = require('fastclick');
+attachFastClick.attach(document.body);
 
 ReactDOM.render(
   <App />,
